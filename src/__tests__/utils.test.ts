62d6953a75778be789ce6e68750878eec0173ec8
Andrew Taylor <ataylorme@users.noreply.github.com>
2023-01-13 07:10:44 -0800

    Add optional installation id input and token output (#2)
    * When the input `installation_id` is provided an installation token is set as an `installation_token` output
    * Update dependencies
    * Version 1.1.0

--- src/__tests__/utils.test.ts
+++ src/__tests__/utils.test.ts
@@ -1,5 +1,6 @@
-import constants from '../constants'
-const {core} = constants
+// https://github.com/actions/toolkit/tree/main/packages/core
+import * as core from '@actions/core'
+
 import {wait, failAndExit} from '../utils'
 
 let mockExit: jest.SpyInstance
@@ -20,10 +21,19 @@ afterAll(() => {
 
 describe('Verify failAndExit', () => {
   const errorMessage = 'Something went wrong!'
+
   it('Fails and exits', () => {
     failAndExit(errorMessage)
     expect(mockCoreError).toHaveBeenCalledWith(errorMessage)
     expect(mockCoreFailed).toHaveBeenCalledWith(errorMessage)
+    expect(mockExit).toHaveBeenCalledWith(1)
+  })
+
+  it('Fails and exits with thrown Error', () => {
+    const thrownError = 'Thrown error message!'
+    failAndExit(errorMessage, new Error(thrownError))
+    expect(mockCoreError).toHaveBeenCalledWith(`${errorMessage}\n${thrownError}`)
+    expect(mockCoreFailed).toHaveBeenCalledWith(`${errorMessage}\n${thrownError}`)
     expect(mockExit).toHaveBeenCalledWith(1)
   })
 })
